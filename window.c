46ab950426fc45d760054fd18be59e58bb43a30b
Henry P <git@henrypaegel.de>
2022-01-06 23:29:56 +0100

    Wrote basic SDL2 stuff to handle the game and update the window.

--- window.c
+++
@@ -1,6 +0,0 @@
-//
-// Created by hpae on 06.01.22.
-//
-// Contains functions to handel SDL2 graphics.
-
-#include "window.h"
--- window.h
+++ window.h
@@ -5,4 +5,35 @@
 #ifndef CCONNECT4_WINDOW_H
 #define CCONNECT4_WINDOW_H
 
+#include "game.h"
+#include <SDL2/SDL.h>
+
+/* ---DEFINITIONS--- */
+
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
+
+void catchPollEvent(SDL_Event e, int* quit);
+void renderGame(SDL_Renderer *renderer, const game_t *game);
+
+
+/* ---IMPLEMENTATIONS--- */
+
+void catchPollEvent(SDL_Event e, int* quit) {
+    while (SDL_PollEvent(&e)) {
+        switch (e.type) {
+            case SDL_QUIT:
+                *quit = 1;
+                break;
+
+            default: {
+            }
+        }
+    }
+}
+
+void renderGame(SDL_Renderer *renderer, const game_t *game) {
+
+}
+
 #endif //CCONNECT4_WINDOW_H
