6eb14ef7b0f2a07109b3991108ac127dcbde4a85
hpae - thinkpad <henry@paegel.net>
2022-01-06 18:18:40 +0100

    Cleaned files more.

--- main.c
+++ main.c
@@ -2,4 +2,10 @@
 // Created by hpae on 02.01.22.
 //
 
+#include "window.h"
 #include "main.h"
+
+int main(void) {
+
+  return 0;
+}
---
+++ window.h
@@ -0,0 +1,8 @@
+//
+// Created by hpae on 06.01.22.
+//
+
+#ifndef CCONNECT4_WINDOW_H
+#define CCONNECT4_WINDOW_H
+
+#endif //CCONNECT4_WINDOW_H
